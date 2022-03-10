df7d918c54886f374461c3917718298d06f60726
chanwoo <chocw0402@gmail.com>
2022-03-10 21:31:04 +0900

    [ADD] UserResponse 추가

--- src/interface/authDTO.ts
+++ src/interface/authDTO.ts
@@ -1,18 +1,21 @@
-export interface SignupDTO {
-    readonly email: string;
-    readonly password: string,
-    readonly nickname?: string;
-}
-
-
 export interface LoginDTO {
     readonly email: string;
     readonly password: string,
 }
 
+export interface SignupDTO extends LoginDTO {
+    readonly nickname?: string;
+}
 
 export interface TokenDTO {
     readonly accessToken: string;
     readonly refreshToken?: string;
 }
 
+
+export interface UserResponse {
+    readonly id: number;
+    readonly social: string;
+    readonly email: string;
+    readonly nickname: string;
+}
