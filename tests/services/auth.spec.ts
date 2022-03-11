1e64b71d0eb8eef4c8fa2564ad94ab907df51c0c
chanwoo <chocw0402@gmail.com>
2022-03-11 20:38:49 +0900

    initial test code

---
+++ tests/services/auth.spec.ts
@@ -0,0 +1,50 @@
+import assert from "assert";
+
+import AuthService from "../../src/services/authService"
+import { User } from "../../src/models"
+import { SignupDTO } from "../../src/interface/authDTO";
+import Error from "../../src/constant/responseError";
+
+
+
+describe("/api/auth", () => {
+    const authServiceInstance = new AuthService(User);
+    let testUser: SignupDTO;
+
+    describe("[POST] /signup", () => {
+        it("[400] null value", async() => {
+            testUser= {
+                email: "",
+                password: "",
+                nickname: ""
+            };
+            const user = await authServiceInstance.SignUp(testUser);
+            assert.strictEqual(user, Error.NULL_VALUE);
+        });
+
+
+
+        it("[400] wrong email convention", async() => {
+            testUser = {
+                email: "test1234user.com",
+                password: "1q2w3e4r!",
+                nickname: "테스트"
+            };
+            const user = await authServiceInstance.SignUp(testUser);
+            assert.strictEqual(user, Error.WRONG_EMAIL_CONVENTION)
+        });
+
+
+
+        it("[400] user already exist", async() => {
+            await User.create({
+                email: "test0000@user.com",
+                password: "test1234!",
+                nickname: "테스팅유저"
+            });
+            const user = await authServiceInstance.SignUp(testUser);
+            assert.strictEqual(user, Error.USER_ALREADY_EXIST);
+        });
+    });
+
+});
