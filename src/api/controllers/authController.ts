ac7be69a5a4ddf1fbc473fcba8a3e19e69c86945
chanwoo <chocw0402@gmail.com>
2022-03-15 03:16:55 +0900

    [CHORE] 필요없는 주석 제거

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -2,10 +2,10 @@
 // service 의존성 주입을 하고, dto형식, 응답 형식에 대한 ~
 // import Container from "typedi";
 import { Request, Response } from "express";
-import { LoginDTO, TokenDTO, SignupDTO, SocialDTO } from "../../interface/authDTO";
+import { LoginDTO, TokenDTO, SignupDTO, SocialDTO } from "../../services/auth/dto/authRequest";
 import Error from "../../constant/responseError";
 import { User } from "../../models";
-import AuthService from "../../services/authService";
+import AuthService from "../../services/auth/authService";
 import { ErrorResponse, SuccessResponse } from "../../modules/apiResponse";
 import sc from "../../constant/resultCode";
 import rm from "../../constant/resultMessage";
@@ -18,28 +18,22 @@ const signup = async (req: Request, res: Response) => {
         //const authServiceInstance = Container.get(AuthService);
         const authServiceInstance = new AuthService(User)
         const data = await authServiceInstance.SignUp(signupDto);
-        /** @Error1 필수 요청 값 누락 */
         if (data === Error.NULL_VALUE) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NULL_VALUE);
         }
-        /** @Error2 이메일 형식 오류 */
         if (data === Error.WRONG_EMAIL_CONVENTION) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_EMAIL_CONVENTION);
         }
-        /** @Error3 비밀번호 형식 오류 */
         if (data === Error.WRONG_PASSWORD_CONVENTION) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PASSWORD_CONVENTION);
         }
-        /** @Error4 이미 존재하는 유저 */
         if (data === Error.USER_ALREADY_EXIST) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.USER_ALREADY_EXIST);
         }
-        /** @Error5 유저 생성 실패 */
         if (data === Error.FAIL_SIGNUP) {
             return ErrorResponse(res,sc.BAD_REQUEST,rm.FAIL_SIGNUP);
         }
-        /** @Success 회원가입 성공 */
-        return SuccessResponse(res, sc.CREATED, rm.CREATED_USER, data);
+        SuccessResponse(res, sc.CREATED, rm.CREATED_USER, data);
     } catch (error) {
         console.log(error);
         ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR);
@@ -52,20 +46,16 @@ const login = async (req: Request, res: Response) => {
     try {
         const authServiceInstance = new AuthService(User);
         const data = await authServiceInstance.LogIn(loginDto);
-        /** @Error1 필수 요청 값 누락 */
         if (data === Error.NULL_VALUE) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NULL_VALUE);
         }
-        /** @Error2 존재하지 않는 유저 */
         if (data === Error.NON_EXISTENT_USER) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NO_USER);
         }
-        /** @Error3 비밀번호가 일치하지 않음 */
         if (data === Error.PW_NOT_CORRECT) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.MISS_MATCH_PW);
         }
-        /** @Success 로그인 성공 */
-        return SuccessResponse(res, sc.OK, rm.LOGIN_SUCCESS, data);
+        SuccessResponse(res, sc.OK, rm.LOGIN_SUCCESS, data);
     } catch (error) {
         console.error(error);
         ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR
@@ -80,25 +70,20 @@ const reissueToken = async (req: Request, res: Response) => {
     try {
         const authServiceInstance = new AuthService(User);
         const data = await authServiceInstance.ReissueToken(reissueTokenDto);
-        /** @Error1 필수 요청 값 누락 */
         if (data === Error.NULL_VALUE) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NULL_VALUE);
         }
-        /** @Error2 리프레시 토큰도 만료 => 재로그인 요청 */
         if (data === Error.TOKEN_EXPIRES) {
             return ErrorResponse(res, sc.UNAUTHORIZED, rm.PLEASE_LOGIN_AGAIN);
         }
-        /** @Error3 해당 유저 없음 */
         if (data === Error.NON_EXISTENT_USER) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NO_USER);
         }
-        /** @Success 로그인 성공 */
-        return SuccessResponse(res,sc.OK,rm.REISSUE_TOKEN,data);
+        SuccessResponse(res,sc.OK,rm.REISSUE_TOKEN,data);
     } catch (error) {
         console.log(error);
         ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR);
     }
-    
 };
 
 // 소셜 로그인
@@ -108,7 +93,6 @@ const socialLogin = async (req: Request, res: Response) => {
     try {
         const authServiceInstance = new AuthService(User);
         const data = await authServiceInstance.SocialLogin(tokenDto, socialDto);
-        /** @Error1 필수 요청 값 누락 */
         if (data === Error.NULL_VALUE) {
             return ErrorResponse(res, sc.BAD_REQUEST, rm.NULL_VALUE);
         }
